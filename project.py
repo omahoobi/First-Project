35f85ea712bf4db3270157227a6da28562e4f18c
omahoobi <omahoobi@yahoo.com>
2014-12-24 14:05:42 -0500

    Create project.py
    PONY

---
+++ project.py
@@ -0,0 +1,15 @@
+def func(x):
+    return x
+
+
+amount=float(raw_input("give me input"))
+
+pony_cost = 200.0
+
+if amount < pony_cost:
+    print "You don't have enough money for a pony! WORK MORE!"
+else:
+    print "PONY IS YOURS, CONGRATS FOOL!"
+
+
+  
